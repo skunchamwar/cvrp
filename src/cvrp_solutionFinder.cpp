39143ed6fd4a0fb798fd1a600467eac9e7816396
skunchamwar <kunchamwar.sandeep@gmail.com>
2017-07-01 16:50:12 +0100

    better solution space explorer

--- src/cvrp_solutionFinder.cpp
+++ src/cvrp_solutionFinder.cpp
@@ -1,5 +1,6 @@
 #include "cvrp_solutionFinder.h"
 #include "cvrp_util.h"
+#include <iostream>
 
 namespace cvrp
 {
@@ -94,31 +95,31 @@ void SolutionFinder::crossover(SolutionModel& solution)
     std::vector<int>& subject1 = solution.chromosomes()[crossoverSubject1].clientSequence();
     std::vector<int>& subject2 = solution.chromosomes()[crossoverSubject2].clientSequence();
 
-    int randomNum = Util::generateRandomNumberInRange(1,10);
+    int randomNum = Util::generateRandomNumberInRange(1,13);
     if (randomNum < 8)
     {
         Util::splitAndCascade(subject1, subject2, crossoverPoint);
     }
-    else //if (randomNum < 9)
+    else if (randomNum < 11)
     {
         Util::splitAndFlipCascade(subject1, subject2, crossoverPoint);
     }
-    // else
-    // {
-    //     int crossoverPoint2 = Util::generateRandomNumberInRange(1,smallChromosomeSize-2);
-    //     while (crossoverPoint == crossoverPoint2)
-    //     {
-    //         crossoverPoint2 = Util::generateRandomNumberInRange(1,smallChromosomeSize-2);
-    //     }
-    //     if (crossoverPoint2 < crossoverPoint)
-    //     {
-    //         Util::spliceAndCascade(subject1, subject2, crossoverPoint2, crossoverPoint);
-    //     }
-    //     else
-    //     {
-    //         Util::spliceAndCascade(subject1, subject2, crossoverPoint, crossoverPoint2);
-    //     }
-    // }
+    else
+    {
+        int crossoverPoint2 = Util::generateRandomNumberInRange(1,smallChromosomeSize-2);
+        while (crossoverPoint == crossoverPoint2)
+        {
+            crossoverPoint2 = Util::generateRandomNumberInRange(1,smallChromosomeSize-2);
+        }
+        if (crossoverPoint2 < crossoverPoint)
+        {
+            Util::spliceAndCascade(subject1, subject2, crossoverPoint2, crossoverPoint);
+        }
+        else
+        {
+            Util::spliceAndCascade(subject1, subject2, crossoverPoint, crossoverPoint2);
+        }
+    }
 
     for (auto &chromosome : solution.chromosomes())
     {
@@ -128,7 +129,9 @@ void SolutionFinder::crossover(SolutionModel& solution)
 
 SolutionModel SolutionFinder::solutionWithEvolution()
 {
+    SolutionModel finalSolution;
     SolutionModel solution;
+    //Util::randomShuffleElements(m_dnaSequence);
     getSolution(solution);
 
     std::vector<SolutionModel> generation;
@@ -136,7 +139,7 @@ SolutionModel SolutionFinder::solutionWithEvolution()
     solutions.push_back(solution);
     double leastCost = solutionCost(solution);
     int x = 0;
-    while (x < 100)
+    while (x < 500)
     {
         bool foundBetterGeneration = false;
         for(auto &sol : solutions)
@@ -151,9 +154,17 @@ SolutionModel SolutionFinder::solutionWithEvolution()
                 {
                     if (currSolCost < leastCost)
                     {
-                        generation.push_back(newSol);
                         leastCost = currSolCost;
+                        finalSolution = newSol;
+                        // newSol.printSolution();
+                        // std::cout << "Ttal Cost: " << solutionCost(newSol) << std::endl;
+                        // std::cout << "##################################################################" << std::endl;
+                    }
+                    if (currSolCost < (leastCost + 40.0))
+                    {
+                        generation.push_back(newSol);
                         foundBetterGeneration = true;
+
                     }
                 }
             }
@@ -167,7 +178,7 @@ SolutionModel SolutionFinder::solutionWithEvolution()
         ++x;
     }
 
-    return *solutions.begin();
+    return finalSolution;
 }
 
 }//cvrp namespace
